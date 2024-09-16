8ba04e2a49e4cf2a3294ba366614713cbd596c17
theskylighter <shiv.27india.com>
2024-09-17 00:22:57 +0530

    Refactor SCC algorithm for better readability and maintainability

--- Application of DFS/*SCC (redo)/scc.cpp
+++ Application of DFS/*SCC (redo)/scc.cpp
@@ -5,83 +5,67 @@
 #include <stack>
 using namespace std;
 
-void dfs(int node, unordered_map<int, bool> &visited, unordered_map<int, list<int>> &adj, stack<int> &st){
+// Depth-First Search to fill the stack with nodes in the order of their finishing times
+void dfs(int node, unordered_map<int, bool> &visited, unordered_map<int, list<int>> &adj, stack<int> &st) {
     visited[node] = true;
-    for(auto i : adj[node]){
-        if(!visited[i]){
-            dfs(i,visited,adj,st);
+    for (auto i : adj[node]) {
+        if (!visited[i]) {
+            dfs(i, visited, adj, st);
         }
     }
-    st.push(node);
+    st.push(node); // Push the node to the stack after visiting all its neighbors
 }
 
-void revdfs(int node, unordered_map<int, list<int>> &rev_adj, stack<int> &st, unordered_map<int, bool> &visited, vector<int> &temp){
+// Reverse DFS to collect all nodes in the current SCC
+void revdfs(int node, unordered_map<int, list<int>> &rev_adj, stack<int> &st, unordered_map<int, bool> &visited, vector<int> &temp) {
     visited[node] = true;
-    temp.push_back(node);
-    for(auto nbr : rev_adj[node]){
-        if(!visited[nbr]){
-            revdfs(nbr,rev_adj,st,visited,temp);
+    temp.push_back(node); // Add the node to the current SCC
+    for (auto nbr : rev_adj[node]) {
+        if (!visited[nbr]) {
+            revdfs(nbr, rev_adj, st, visited, temp);
         }
     }
 }
 
-vector<vector<int>> SCC(vector<vector<int>> &edges, int v){
-    
-    //  Creating ADJACENCY  LIST
+// Function to find all Strongly Connected Components (SCCs) in a directed graph
+vector<vector<int>> SCC(vector<vector<int>> &edges, int v) {
+    // Creating ADJACENCY LIST
     unordered_map<int, list<int>> adj;
     stack<int> st;
-    for(auto edge : edges){
+    for (auto edge : edges) {
         int u = edge[0];
         int v = edge[1];
-
-        adj[u].push_back(v);
+        adj[u].push_back(v); // Add edge to the adjacency list
     }
 
+    // Step 1: Perform DFS on the original graph to fill the stack
     unordered_map<int, bool> visited;
-    vector<vector<int>> ans;
-
-    for(int i = 0; i < v; i++){
-        if(!visited[i]){
-            dfs(i,visited,adj,st);
+    for (int i = 0; i < v; i++) {
+        if (!visited[i]) {
+            dfs(i, visited, adj, st);
         }
     }
 
-    //creating REVERSED adj list
+    // Step 2: Transpose the graph
     unordered_map<int, list<int>> rev_adj;
-    for(int i = 0; i < v; i++){
-        visited[i] = false;
-        for(auto j : adj[i]){
-            rev_adj[j].push_back(i);
-        }
+    for (auto edge : edges) {
+        int u = edge[0];
+        int v = edge[1];
+        rev_adj[v].push_back(u); // Reverse the direction of the edge
     }
 
-    while(!st.empty()){
-        int top = st.top();
+    // Step 3: Perform DFS on the transposed graph
+    visited.clear();
+    vector<vector<int>> sccs;
+    while (!st.empty()) {
+        int node = st.top();
         st.pop();
-        if(!visited[top]){
+        if (!visited[node]) {
             vector<int> temp;
-            revdfs(top,rev_adj,st,visited,temp);
-            ans.push_back(temp);
+            revdfs(node, rev_adj, st, visited, temp);
+            sccs.push_back(temp); // Add the current SCC to the list of SCCs
         }
     }
 
-    return ans;
-}
-
-int main()
-{
-    int V = 5;
-    vector<vector<int> > edges{{ 0,1}, { 1, 2 }, { 2, 0 }, { 3, 2 }, { 4, 3 }};
-
-    vector<vector<int>> ans;
-    ans = SCC(edges, V);
-    
-    cout << "Strongly Connected Components are:\n";
-    for (auto x : ans) {
-        for (auto y : x) {
-            cout << y << " ";
-        }
-        cout << "\n";
-    }
-    return 0;
+    return sccs; // Return the list of SCCs
 }
