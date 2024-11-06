277e9131d3b51c8d61c82ff7f2d2a794653a5424
Shivam Singh <2023uai1819@mnit.ac.in>
2024-11-06 14:56:16 +0530

    create mergesort code

---
+++ sorting algorithms/mergesort.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+void merge(){
+
+}
+void mergesort(){
+
+}
+
+int main(){
+    vector<int> arr={2,9,3,6,7,12,8,20};
+    
+    return 0;
+}
